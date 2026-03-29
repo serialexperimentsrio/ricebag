6510beb184a3aff695693866f3f3c9f9d6d42be1
Rio <engineer@disroot.org>
2026-03-30 01:31:49 +0600

    Add design tokens and component exports

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+
+export { Box } from './Box'
+export { Button } from './Button'
+export { Dropdown } from './Dropdown'
+export { Icon } from './Icon'
+export { IconButton } from './IconButton'
+export { Loading } from './Loading'
+export { Panel } from './Panel'
+export { Popover } from './Popover'
+export { Select } from './Select'
+export { Tooltip } from './Tooltip'
+
+export { setModal, hideModal, openPreviousModal, registerCloseAnimation, isModalTransitioning, modalActive } from './Modal'
+export { ModalBox } from './Modal/ModalBox'
+export type { ModalBoxProps, ModalBoxComponent } from './Modal/ModalBox'
+
+export { toast, setToastLocation } from './Toast'
+export { ToastBox } from './Toast/ToastBox'
+export type { ToastBoxProps } from './Toast/ToastBox'
